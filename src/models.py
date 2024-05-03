2f8a6e70140b75039802ead93bfd6b5709f37560
Andrés Torres <125112555+andres-stg@users.noreply.github.com>
2024-05-03 20:45:42 +0000

    SW data model finished

--- src/models.py
+++ src/models.py
@@ -7,23 +7,33 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(15), nullable=False)
+    email = Column(String(100), nullable=False)
+    password = Column(String(20), nullable=False)
+    subscription_date = Column('date', nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planets(Base):
+    __tablename__ = 'Planets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    climate = Column(String(250))
+    gravity = Column(String(250), nullable=False)
+    Planets = relationship(User)
+
+class Characters(Base):
+    __tablename__ = 'Characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    movies = Column(String(50))
+    mass = Column(Integer)
+    Characters = relationship(User)
 
     def to_dict(self):
         return {}
